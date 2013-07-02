963ec4d6b97aea8d5d5efbf6b996dad66e491d25
kccpac <colin_ng@hotmail.com>
2013-07-02 15:06:08 -0400

    Update web.js

--- web.js
+++ web.js
@@ -2,5 +2,5 @@
 var fs = require('fs');
 var outfile = "node.txt";
 var out = "A startup is a business built to grow rapidly.\n";
-fs.writeFileSync(outfile, out);  
+#fs.writeFileSync(outfile, out);  
 console.log("Script: " + __filename + "\nWrote: " + out + "To: " + outfile);
